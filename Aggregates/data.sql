cb5182b4091d3f85ab96f74c7c9aaf3fc9f06759
Ola Ishola <olaishola@hotmail.co.uk>
2022-03-23 00:25:21 +0100

    Add more aggregate functions

--- Aggregates/data.sql
+++ Aggregates/data.sql
@@ -4,3 +4,9 @@ INSERT INTO cities VALUES (1, 'Miami'),(2,'Orlando'),
 INSERT INTO users VALUES (1,1,'Jhon','Doe',22),
 (2,1,'Albert','Thomson',15),(3,2,'Robert','Ford',65),(4,3,'Samantha','Simpson',9),(5,2,'Carlos','Bennet',42),
 (6,2,'Mirtha','Lebrand',81),(7,3,'Alex','Gomez',31);
+
+
+INSERT INTO users (id, city_id, firs_name, last_name, age)
+VALUES (8, 2, 'Frederic', 'Scott',NULL);
+INSERT INTO users (id, city_id, firs_name, last_name, age)
+VALUES (9, 4, 'Stacy', 'Roberts',NULL);
--- Aggregates/queries.sql
+++ Aggregates/queries.sql
@@ -12,3 +12,50 @@ SELECT AVG(age) FROM users;
 SELECT COUNT(age) FROM users; -- for counting the number of non null values
 
 SELECT COUNT(*) FROM users; -- the actual number of table rows regardless of the column value
+
+-- Group by with aggregate functions
+
+SELECT city_id, MIN(age) FROM users GROUP BY city_id;
+SELECT city_id, MAX(age) FROM users GROUP BY city_id;
+SELECT city_id, SUM(age) FROM users GROUP BY city_id;
+SELECT city_id, COUNT(age) FROM users GROUP BY city_id
+
+-- Calculating average
+SELECT
+    city_id,
+    SUM(age),
+    COUNT(age),
+    SUM(age) / COUNT(age) as average
+FROM
+    users
+GROUP BY city_id;
+
+
+SELECT
+    city_id,
+    AVG(age)
+FROM
+    users
+GROUP BY city_id;
+
+
+SELECT
+    city_id, COUNT(age), AVG(age)
+FROM
+    users
+GROUP BY city_id
+HAVING AVG(age) >= 20
+
+
+SELECT
+    city_id, COUNT(age), AVG(age)
+FROM
+    users
+WHERE last_name <> 'Simpson' -- Excluding this user name 
+GROUP BY city_id
+HAVING AVG(age) >= 20
+
+
+-- Dealing with null values
+
+
